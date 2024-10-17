d8741b4a0095f47259d619e6daafe10e62ded85f
GabrieleKM <126702987+GabrieleKM@users.noreply.github.com>
2024-10-17 11:31:30 -0300

    Add files via upload

--- graficos/common.js
+++ graficos/common.js
@@ -8,5 +8,24 @@ const tickConfig = {
     size: 16,
     color: getCSS('--primary-color')
 }
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config);
+}
 
-export {getCSS, tickConfig}
+// código omitido
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    container.appendChild(paragrafo)
+}
+
+export { getCSS, tickConfig, criarGrafico, incluirTexto }
